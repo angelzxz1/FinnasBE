e2e658f71b4eddcfd68ccded0c6d95a05fc408bb
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-04-24 17:25:14 -0500

    changed the model to be only strings, this might change in future

--- src/database/models/purchase.js
+++ src/database/models/purchase.js
@@ -1,31 +1,30 @@
-import { DataTypes } from 'sequelize'
-import conn from '../conn.js'
+import { DataTypes } from "sequelize";
+import conn from "../conn.js";
 
 const Purchase = conn.define(
-    'Purchase',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        date: {
-            type: DataTypes.DATEONLY,
-            allowNull: false,
-        },
-        hour: {
-            type: DataTypes.TIME,
-            allowNull: false,
-        },
-        amountSpent: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-        },
-    },
-    {
-        freezeTableName:true
-    }
-)
+	"Purchase",
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		date: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		hour: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		amountSpent: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		freezeTableName: true,
+	}
+);
 
-export default Purchase
-
+export default Purchase;
